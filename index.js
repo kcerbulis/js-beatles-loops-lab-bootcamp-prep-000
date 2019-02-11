e25c4d59ab49e4766d013620f14b1b5933caca60
reactive-queue-1834 <kcerbulis@gmail.com>
2019-02-11 23:07:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// add solution here
+const musician = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+
+var musicianArray = [];
+
+
+
+
+
+function theBeatlesPlay(musician, instruments){
+  for (let i = 0; i <= 3; ++i ){
+    musicianArray.push(musician[i] + " plays " + instruments[i])
+  }
+  return musicianArray
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var k = [];
+
+
+function iLoveTheBeatles(num){
+  do {
+  k.push("I love the Beatles!");
+  ++num
+} while (num < 15);
+  return k
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
